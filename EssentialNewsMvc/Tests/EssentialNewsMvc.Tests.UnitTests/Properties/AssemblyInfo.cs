84cfe0dfeb6f4240d40fa09cab830876e10257aa
Asparuh <puhabg@live.com>
2017-10-09 17:14:48 +0300

    Adds unit test project

---
+++ EssentialNewsMvc/Tests/EssentialNewsMvc.Tests.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialNewsMvc.Tests.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialNewsMvc.Tests.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7bd82af3-366e-40c4-ab1a-b02ee1dc0400")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialNewsMvc/Tests/EssentialNewsMvc.Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace EssentialNewsMvc.Tests.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That(true, Is.EqualTo(true), "Simple Test");
+        }
+    }
+}
