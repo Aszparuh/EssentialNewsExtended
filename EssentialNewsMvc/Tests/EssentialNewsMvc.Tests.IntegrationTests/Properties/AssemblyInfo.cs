bcfa8c7b87f3ae9ae4e4265e7db94a94cb3e4215
puhabg <puhabg@live.com>
2017-10-15 22:24:14 +0300

    Adds project for integration tests

---
+++ EssentialNewsMvc/Tests/EssentialNewsMvc.Tests.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialNewsMvc.Tests.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialNewsMvc.Tests.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0aa16b10-d9f4-4994-ab8b-8a1715d7531a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialNewsMvc/Tests/EssentialNewsMvc.Tests.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace EssentialNewsMvc.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
